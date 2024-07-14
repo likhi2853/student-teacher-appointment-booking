fed54b872847fcd6de64988418ca7cc979161eac
likhi2853 <158829162+likhi2853@users.noreply.github.com>
2024-07-14 11:30:09 +0530

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,4 @@
+export function logAction(action, user) {
+    console.log(`Action: ${action}, User: ${user}`);
+    // Further logging implementation to save logs in Firebase or other logging service
+}
